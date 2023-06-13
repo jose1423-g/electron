bba67696901b2831f8f46dd6be1e3dcbd3a116c1
unknown <cubobale1423@gmail.com>
2023-06-13 10:20:33 -0500

    agregamos el modal

--- js/renderer.js
+++ js/renderer.js
@@ -2,15 +2,28 @@ const dbclick = document.getElementById('logo-kisko')
 const logo  = document.getElementById('logo-gasofac')
 const texto_bienvenida  = document.getElementById('texto-bienvenida')
 const icon = document.getElementById('info')
+const btn_close = document.getElementById('btn_close')
+const modal = document.getElementById('modal_local')
+
+const carouselContainer = document.querySelector('.carousel-container');
+const carouselItems = document.querySelectorAll('.carousel-item1');
+const prevButton = document.getElementById('prev-button');
+const nextButton = document.getElementById('next-button');
 
 dbclick.addEventListener('dblclick', function () {
     window.location.href = "login.html"  
 })
 
-icon.addEventListener('click', info)
+icon.addEventListener('click', Removeclass)
+btn_close.addEventListener('click', Addclass)
 
-function info() {
-    // console.log("diste click al info")
+
+function Removeclass() {
+    modal.classList.remove('none')
+}
+
+function Addclass() {
+    modal.classList.add('none')
 }
 
 function datos() {
@@ -34,3 +47,31 @@ function datos() {
     });
 }
 onload = datos
+
+let currentIndex = 0;
+
+prevButton.addEventListener('click', showPreviousItem);
+nextButton.addEventListener('click', showNextItem);
+
+//muestra el anterior elemento
+function showPreviousItem() {
+  currentIndex = (currentIndex - 1 + carouselItems.length) % carouselItems.length;
+  updateCarousel()
+}
+
+//muestra el siguiente elemento 
+function showNextItem() {
+  currentIndex = (currentIndex + 1) % carouselItems.length;
+  updateCarousel()
+}
+
+// offsetWidth = propiedad de solo lectura en JavaScript que devuelve el ancho total de un elemento HTML
+//reinicia el carrusel
+function updateCarousel() {
+  const itemWidth = carouselItems[0].offsetWidth;
+  carouselContainer.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
+}
+
+// Actualizar el carrusel inicialmente
+updateCarousel()
+
