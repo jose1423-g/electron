ab6a18454467a3b9416cf86f58994f6017157502
unknown <cubobale1423@gmail.com>
2023-07-11 18:25:37 -0500

    version4.3

--- teclado.js
+++ teclado.js
@@ -3,6 +3,7 @@ const client = require('electron-virtual-keyboard/client.js');
 var jQuery = $ = require('jquery');
 require('electron-virtual-keyboard/client.js')(window, jQuery);
 
+let RFC = document.getElementById('RFC');
 
 const teclado = $('.keyboard').keyboard({
   theme: '',
@@ -11,12 +12,18 @@ const teclado = $('.keyboard').keyboard({
   displayOnFocus: true,
 })
 
+const teclado2 = $('.keyboard2').keyboard({
+  theme: '',
+  layout: 'us-en', 
+  autoPosition: true,
+  displayOnFocus: true
+})
+
 //quita el teclado al darle enter al rfc
-const key_rfc = document.getElementById('RFC')
-if (key_rfc) {
-  key_rfc.addEventListener('keydown', function (e) {
+if (RFC) {
+  RFC.addEventListener('keydown', function (e) {
       if (e.key === 'Enter') {
         teclado.hide()
       }
-  })
+    })
 }
